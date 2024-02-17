140c517a9c0e1840420a3b90900c6bfd90055f83
Richard Ly <rly758@gmail.com>
2024-02-17 03:14:21 -0800

    Completed getBookCard()

--- library.js
+++ library.js
@@ -5,7 +5,6 @@ function createLibrary(libContainer) {
     //create html div with class="library"
     const lib = document.createElement("div");
     lib.className = "library";
-    lib.textContent = "test";
     libContainer.appendChild(lib);
 }
 
@@ -17,7 +16,7 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.getDetails = function() {
-    return {title, author, pages, read};
+    return {title:this.title, author:this.author, pages:this.pages, read:this.read};
 };
 
 //on form submission add a new book to myLibrary, and display the new book card on page
@@ -36,6 +35,28 @@ function showLibrary() {
     //append Book card element to library (html div element)
 }
 
+function getBookCard(book) {
+    const details = book.getDetails();
+    const card = document.createElement("div");
+    card.className = "book";
+    for (property in details) {
+        const prop = `${property}`[0].toUpperCase() + `${property}`.slice(1);
+        const val = details[property];
+
+        const p = document.createElement("p");
+        p.className = property;
+        p.textContent = prop + ": " + val;
+
+        card.appendChild(p);
+    }
+
+    return card;
+}
+
 const book1 = new Book("Book Title","Arthor Thor","172",true);
+myLibrary.push(book1);
 
 createLibrary(libContainer);
+const lib = document.querySelector(".library");
+const testCard = getBookCard(book1);
+lib.appendChild(testCard);
